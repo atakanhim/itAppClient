5ba8c9485432e213cfaf937d158eba66c7a2aa84
Atakan Him <52455771+atakanhim@users.noreply.github.com>
2024-04-20 04:29:10 +0300

    oteleme işlemi yapıldı

--- src/app/pages/insidepages/employees/edit-employee/edit-employee.component.ts
+++ src/app/pages/insidepages/employees/edit-employee/edit-employee.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ElementRef, Renderer2, ViewChild, ViewContainerRef } from '@angular/core';
 
 import { ChangeDetectorRef } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
@@ -17,6 +17,8 @@ import { List_CheckMarks_Employee } from 'src/app/contracts/checkmark/list_check
 import {  EmployeWithNothingVM, EmployeeWithCheckMarkVM } from 'src/app/contracts/employee/employe_vm';
 import { List_CheckMark } from 'src/app/contracts/checkmark/list_checkmark';
 import {  NgIf } from '@angular/common';
+declare var $: any; // jQuery'yi kullanabilmek için bu deklarasyonu ekleyin
+
 @Component({
     selector: 'app-edit-employee',
     standalone: true,
@@ -24,32 +26,83 @@ import {  NgIf } from '@angular/common';
     styleUrl: './edit-employee.component.scss',
     imports: [MatTableModule, MatPaginatorModule, CommonModule, MatIconModule, DeleteDirective, LongdatePipe]
 })
+
 export class EditEmployeeComponent {
   currentDate = new Date(); // Şu anki tarihi alır
   currentMonth: number;
   empbilgi :any;
   days = ["Pt","Sl","Ça","Pe","Cu","Ct","Pa"];
+  dayOfWeek: number[] = [0, 1, 2, 3, 4, 5, 6]; // Bu diziyi günlerin dizisine göre ayarlayın
+  // checkmarks ve addCheckMark fonksiyonunuzu buraya ekleyin
   checkmarks:CheckMarkWithEmployeeVM[];
   empId : string;
-  constructor(private route:ActivatedRoute,private checkmarkService:CheckmarksService,private toastrService:CustomToastrService,) {
+  @ViewChild('checkmarkDiv', { read: ViewContainerRef }) checkmarkDiv: ViewContainerRef;
+
+  constructor(private route:ActivatedRoute,private checkmarkService:CheckmarksService,private toastrService:CustomToastrService,private elementRef:ElementRef,private renderer: Renderer2) {
     this.currentMonth = this.getCurrentMonth(this.currentDate);
   }
  async ngOnInit() {
+
     await this.route.params.subscribe(async (params) => {
        this.empId = params['employeeId'];
     });
+
   }
   getCurrentMonth(date: Date): number {
-    const monthNames = ["Ocak", "Şubat", "Mart", "Nisan", "Mayıs", "Haziran",
-                        "Temmuz", "Ağustos", "Eylül", "Ekim", "Kasım", "Aralık"];
     return  date.getMonth()+1;
   }
-
-
+  calculateOffset(index: number, startDay: any): number {
+    const dayOffset = (index + startDay) % 7;
+    return dayOffset === 0 ? 0 : dayOffset - 1; // Pazartesi başlangıç olduğu için 0'dan başlamasını sağlayın
+  }
+  ngAfterViewChecked() {
+    // checkmarks varsa ve içeriği dolu ise
+    if (this.checkmarks && this.checkmarks.length > 0) {
+      // Template True çalıştığında
+      const childDiv = document.querySelector('.templateTrue');
+      let firstChild = childDiv.firstChild as Element; // Element olarak tip dönüşümü
+      if (childDiv && firstChild) {
+        // Template True çalıştığında
+        let dateStr = firstChild.getAttribute('data-date'); // data-date özelliğine erişme
+        let dateObj = new Date(dateStr); // Tarihi Date nesnesine dönüştür
+        let dayNumber = dateObj.getDay(); // Haftanın günü adını al
+        let marginFactor = 0; // Varsayılan değer
+        switch (dayNumber) {
+          case 1: // Pazartesi
+            marginFactor = 0;
+            break;
+          case 2: // Salı
+            marginFactor = 1;
+            break;
+          case 3: // Çarşamba
+            marginFactor = 2;
+            break;
+          case 4: // Perşembe
+            marginFactor = 3;
+            break;
+          case 5: // Cuma
+            marginFactor = 4;
+            break;
+          case 6: // Cumartesi
+            marginFactor = 5;
+            break;
+          case 0: // Pazar
+            marginFactor = 6;
+            break;
+        }
+        const marginLeftValue = marginFactor * 65; 
+        this.renderer.setStyle(childDiv, 'margin-left', `${marginLeftValue}px`); // margin-left ayarla
+      } 
+    } 
+  }
   async ngAfterViewInit() {
     //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
     //Add 'implements AfterViewInit' to the class.
+
+    
     await this.loadCheckMarks();
+
+
   }
   async addCheckMark(){
     try{
@@ -70,14 +123,18 @@ export class EditEmployeeComponent {
     let response:List_CheckMarks_Employee = await this.checkmarkService.getCheckmarksWithEmployeeId(this.empId,this.currentMonth);
     if (response.checkMarks.length>0){
       console.log(response.checkMarks);
+      
       this.checkmarks=response.checkMarks;
       this.empbilgi = {
         name: this.checkmarks[0].employee.employeName,
         surname: this.checkmarks[0].employee.employeSurname,
         telno: this.checkmarks[0].employee.employeTelNo,
-        departman: this.checkmarks[0].employee.department.name,
-        
+        departman: this.checkmarks[0].employee.department.name, 
       }
+      // bu kısımda checkmarks[0] ilk elemanına bakarak otele class first item margin left vermek istiyorum 
+      // let element = $(this.elementRef.nativeElement).find(".templateFalse")
+      // console.log($(element));
+      
     }
     else{
       // puantaj boş ise
